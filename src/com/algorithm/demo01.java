2a8007b2551a124c02931be3a06a42779f03272e
guoxinchao <guoxinchao.126@gmail.com>
2023-08-31 08:57:51 +0800

    算法项目初始化

---
+++ src/com/algorithm/demo01.java
@@ -0,0 +1,13 @@
+package com.algorithm;
+
+/**
+ * @author :guoxinchao
+ * @date :Created in 2023/8/31 8:57
+ * @description:
+ * @version:
+ */
+public class demo01 {
+    public static void main(String[] args) {
+        System.out.println("算法项目初始化");
+    }
+}
